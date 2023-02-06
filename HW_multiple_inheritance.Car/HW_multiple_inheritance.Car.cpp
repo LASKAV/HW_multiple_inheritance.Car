48e741cbeb7cd521b1cb617d32f2446c321b2bfd
LASKAV <mksidoops@gmail.com>
2023-02-06 17:27:51 +0200

    Add project files.

---
+++ HW_multiple_inheritance.Car/HW_multiple_inheritance.Car.cpp
@@ -0,0 +1,103 @@
+/*
+»спользу€ множественное наследование, разработать класс УјвтомобильФ.
+јвтомобиль будет потомком дл€ классов У олесаФ, УƒвигательФ, УƒвериФ.
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Wheels
+{
+protected:
+    int numWheels;
+    int wheelSize;
+
+public:
+    Wheels()
+    {
+        numWheels = 4;
+        wheelSize = 16;
+    }
+
+    void show_Wheels()
+    {
+        cout
+            << "___Wheels___" << endl
+            << " Num wheels = " << numWheels << endl
+            << " Size = " << wheelSize << endl;
+    }
+};
+
+class Engine
+{
+protected:
+    int numCylinders;
+    int horsepower;
+public:
+    Engine()
+    {
+        numCylinders = 6;
+        horsepower = 300;
+    }
+    void show_Engine()
+    {
+        cout
+            << "___Engine___" << endl
+            << " Num cylinders = " << numCylinders << endl
+            << " Horsepower = " << horsepower << endl;
+    }
+};
+
+class Doors
+{
+protected:
+    int numDoors;
+    int doorSize;
+public:
+    Doors()
+    {
+        numDoors = 4;
+        doorSize = 30;
+    }
+
+    void show_Doors()
+    {
+        cout
+            << "___Doors___" << endl
+            << " Num doors = " << numDoors << endl
+            << " Door Size = " << doorSize << endl;
+    }
+};
+
+class Car : public Wheels, public Engine, public Doors
+{
+protected:
+    string color;
+    string model;
+public:
+    Car()
+    {
+        color = "Black";
+        model = "AUDI";
+    }
+    void show_Car()
+    {
+        cout
+            << "___Car___" << endl
+            << " Color = " << color << endl
+            << " Model = " << model << endl;
+
+        Wheels::show_Wheels();
+        Engine::show_Engine();
+        Doors::show_Doors();
+    }
+};
+
+int main()
+{
+    Car myCar;
+    myCar.show_Car();
+
+    return 0;
+}
